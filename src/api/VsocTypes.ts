ba9d0fdf61c009ee322c54c1eacedb3eb1df7f56
thien123abc <thienngov25081987@gmail.com>
2024-10-25 15:33:13 +0700

    Create VsocTypes.ts

---
+++ src/api/VsocTypes.ts
@@ -0,0 +1,106 @@
+export type VsocUserAction = 'WAIT' | 'DONE' | 'PROMT';
+export type VsocMessageType = 'text' | 'break_paragraph';
+export type VsocConversationType = 'QA' | 'monitor';
+
+export interface IVsocApiResult<T> {
+  status: number;
+  sub_status?: number;
+  description?: string;
+  result?: T;
+}
+
+export interface IVsocCreateConversationArgs {
+  text: string;
+  type: VsocConversationType;
+}
+
+export interface IVsocSendMessageArgs {
+  text: string;
+  conversation_id: string;
+}
+
+export interface IVsocCreateConversationResult {
+  conversation_id: string;
+  action: VsocUserAction;
+}
+
+export interface IVsocGetNextMessageArgs {
+  conversation_id: string;
+}
+
+export interface IVsocGetNextMessageResult extends IVsocStoredMessage {
+  role: string;
+}
+
+export interface IVsocStoreMessageArgs {
+  message: IVsocStoredMessage;
+}
+
+export interface IVsocStoredMessage {
+  conversation_id: string;
+  action: VsocUserAction;
+  role: string;
+  message: string;
+  type: VsocMessageType;
+  time: number;
+}
+
+export interface IVsocStoreConversationArgs {
+  conversation_id: string;
+  title?: string;
+}
+
+export interface IVsocListStoredConversationsArgs {
+  limit?: number;
+  pinned?: boolean;
+}
+
+export interface IVsocStoredConversation {
+  id: string;
+  title: string;
+  time: number;
+}
+
+export interface IVsocGetSettingsArgs {}
+
+export interface IVsocMonitorSetting {
+  pathPattern: string;
+  contentDomPath: string;
+  injectedDomPath: string;
+}
+
+export interface IVsocSetting {
+  domain: string;
+  monitor: IVsocMonitorSetting[];
+}
+
+export interface IVsocGetSettingResult extends IVsocSetting {}
+
+export interface IVsocMessageQuery {
+  conversation_id: string;
+  limit?: number;
+  from?: number;
+  to?: number;
+}
+
+export interface IVsocListStoredMessagesArgs extends IVsocMessageQuery {}
+
+export interface IChromeMessage {
+  type: string;
+  [key: string]: any;
+}
+export interface IChromeResponseMessage {
+  type?: string;
+  error?: boolean;
+  [key: string]: any;
+}
+
+export interface IChromeGetSettingsMessageResult extends IChromeResponseMessage {
+  value: IVsocSetting[];
+}
+
+export interface IVsocRole {
+  color: string;
+  avatar: string;
+  background_color: string;
+}
