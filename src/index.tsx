c58d4fd1c01ef433418ccaf42a81e08c5fd95d5e
thien123abc <thienngov25081987@gmail.com>
2024-10-25 16:01:57 +0700

    Create index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,24 @@
+import { render } from 'react-dom';
+
+import App from './pages/App';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from '@mui/material';
+import { store } from './store/store';
+import { theme } from './theme';
+import './assets/css/index.scss';
+
+import api from './api/VsocApi';
+
+(window as any).api = api;
+
+const VsocApp = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
+  );
+};
+
+render(<VsocApp />, document.getElementById('root'));
