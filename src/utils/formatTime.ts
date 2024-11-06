0b7b044683d7ffe13a9c096a8f5188acbf483ef8
thien123abc <thienngov25081987@gmail.com>
2024-11-06 18:49:01 +0700

    Update formatTime.ts

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -17,8 +17,14 @@ export const formattedTime = ({ lastUsedTime }: UseFormattedTimeProps): string =
     const diffInMonths =
       now.getMonth() - lastUsedDate.getMonth() + 12 * (now.getFullYear() - lastUsedDate.getFullYear());
 
+    // Kiểm tra xem thời gian gửi có phải là trong ngày hôm nay không
+    const isToday =
+      now.getFullYear() === lastUsedDate.getFullYear() &&
+      now.getMonth() === lastUsedDate.getMonth() &&
+      now.getDate() === lastUsedDate.getDate();
+
     // Tin nhắn gửi trong ngày
-    if (diffInDays < 1) {
+    if (diffInDays < 1 && isToday) {
       if (diffInMinutes < 1) {
         return 'Vừa xong';
       } else if (diffInMinutes < 60) {
@@ -31,28 +37,30 @@ export const formattedTime = ({ lastUsedTime }: UseFormattedTimeProps): string =
     }
 
     // Tin nhắn gửi trong 1 tuần
-    if (diffInDays < 7) {
+    if (diffInDays < 7 && !isToday) {
       const daysOfWeek = ['Chủ nhật', 'Thứ hai', 'Thứ ba', 'Thứ tư', 'Thứ năm', 'Thứ sáu', 'Thứ bảy'];
       return daysOfWeek[lastUsedDate.getDay()]; // Hiển thị thứ trong tuần
     }
 
     // Tin nhắn gửi trong cùng tháng nhưng cách hơn một tuần
-    const currentMonth = now.getMonth();
+    // Tin nhắn gửi cách hơn một tuần nhưng khác tháng
+    // Tin nhắn gửi trong các tháng trước trong cùng năm
+    // const currentMonth = now.getMonth();
     const lastUsedMonth = lastUsedDate.getMonth();
-    if (currentMonth === lastUsedMonth && now.getFullYear() === lastUsedDate.getFullYear()) {
+    if (now.getFullYear() === lastUsedDate.getFullYear()) {
       const day = lastUsedDate.getDate();
       const month = lastUsedMonth + 1;
       return `${day} tháng ${month}`;
     }
 
     // Tin nhắn gửi trong các tháng trước trong cùng năm
-    const currentYear = now.getFullYear();
-    const lastUsedYear = lastUsedDate.getFullYear();
-    if (currentYear === lastUsedYear && diffInMonths > 1) {
-      const day = lastUsedDate.getDate();
-      const month = lastUsedDate.getMonth() + 1;
-      return `${day} tháng ${month}`;
-    }
+    // const currentYear = now.getFullYear();
+    // const lastUsedYear = lastUsedDate.getFullYear();
+    // if (currentYear === lastUsedYear && diffInMonths > 1) {
+    //   const day = lastUsedDate.getDate();
+    //   const month = lastUsedDate.getMonth() + 1;
+    //   return `${day} tháng ${month}`;
+    // }
 
     // Tin nhắn gửi trong các năm trước
     const year = lastUsedDate.getFullYear();
