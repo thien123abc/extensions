94030323fde8bc0bd2af31c80545a59591638076
thien123abc <thienngov25081987@gmail.com>
2024-11-09 21:35:21 +0700

    Update declarations.d.ts

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -6,3 +6,14 @@ declare module 'dompurify' {
   const dompurify: any;
   export default dompurify;
 }
+declare module 'prismjs' {
+  const Prism: any;
+  export = Prism;
+}
+
+// src/types/katex.d.ts
+declare module 'katex' {
+  export function render(math: string, element: HTMLElement, options?: any): void;
+  export function renderToString(math: string, options?: any): string;
+  export function version(): string;
+}
