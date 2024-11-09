a842b10ba30c0625403ddc85514a055626804cb2
thien123abc <thienngov25081987@gmail.com>
2024-11-09 21:35:04 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,13 @@ import './assets/css/index.scss';
 
 import api from './api/VsocApi';
 
+import 'prismjs/themes/prism.css'; // Chọn theme Prism.js
+import 'prismjs/components/prism-python.min.js'; //  hỗ trợ Python
+import 'prismjs/components/prism-javascript.min.js'; //  hỗ trợ JavaScript
+import 'prismjs/components/prism-css.min.js'; //  hỗ trợ CSS
+import 'prismjs/components/prism-markup.min.js'; //  hỗ trợ HTML
+import 'katex/dist/katex.min.css';
+
 (window as any).api = api;
 
 const VsocApp = () => {
