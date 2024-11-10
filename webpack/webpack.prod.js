211b49ed1f391bca877554733006b35919dba14b
thien123abc <thienngov25081987@gmail.com>
2024-11-10 21:24:26 +0700

    Update webpack.prod.js

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -3,4 +3,21 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: 'production'
-});  
+});
+
+// webpack/webpack.prod.js hoặc webpack.config.js
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js', // Điểm vào của ứng dụng
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Thư mục output
+    filename: 'bundle.js', // Tên file bundle
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',  // Đảm bảo template HTML đúng
+    }),
+  ],
+};
