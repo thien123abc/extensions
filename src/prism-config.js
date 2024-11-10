fe9fc63bf0df445e4f07947b0a98a91356a151bf
thien123abc <thienngov25081987@gmail.com>
2024-11-10 21:21:48 +0700

    Create prism-config.js

---
+++ src/prism-config.js
@@ -0,0 +1,17 @@
+// src/prism-config.js
+
+// Tải PrismJS chính
+import 'prismjs';  
+
+// Tải theme mặc định của PrismJS
+import 'prismjs/themes/prism.css';  
+
+// Tải các ngôn ngữ mà bạn cần
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-python';
+import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-java';
+import 'prismjs/components/prism-c';
+import 'prismjs/components/prism-csharp';
+import 'prismjs/components/prism-cpp';
+
